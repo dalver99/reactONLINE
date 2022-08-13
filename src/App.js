044fd05dc13a17c101e391d123a621c25954fdae
dalver99 <jykevinkim@gmail.com>
2022-08-13 11:47:14 +0900

    add home Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from "./logo.svg";
+import Navbar from "./Navbar";
 import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <p>Edit and study plz</p>s
+      <Navbar />
+
+      <Home />
+
+      <p>Edit and study plz</p>
     </div>
   );
 }
