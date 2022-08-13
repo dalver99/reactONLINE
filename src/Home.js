222382d27daec757eac859bb481b83b268da4a9b
dalver99 <jykevinkim@gmail.com>
2022-08-13 12:18:31 +0900

    someshit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+  let name = "mario";
+
+  const handleClick = (e) => {
+    name = "luigi";
+    console.log(name);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
+      <button onClick={handleClick}>Click me!</button>
     </div>
   );
 };
