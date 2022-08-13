3e0cdeee1c2e75e8e5b5f790a553a5603a41cb0c
dalver99 <jykevinkim@gmail.com>
2022-08-13 11:34:17 +0900

    modi App

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Edit and study plz</p>
-      </header>
+      <p>Edit and study plz</p>s
     </div>
   );
 }
